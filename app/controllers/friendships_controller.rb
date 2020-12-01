c1a7ead17f8b903505c0752f4e57d6ec2a3832a7
jose-Abel <joseabel.ramirezfrontany@gmail.com>
2020-12-01 12:45:11 -0400

    Create the friendship table. Update user model with the associations to the friendship model and the methods to call those associations.

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,6 @@
+class FriendshipsController < ApplicationController
+
+
+
+    
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,33 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  has_many :friendships
+  has_many :friends, class_name: 'Friendship', foreign_key: :friend_id
+
+  def friends_list
+    friends_array = friendships.map{|friendship| friendship.friend if friendship.confirmed}
+
+    friends_array + friends.map{|friendship| friendship.user if friendship.confirmed}
+    
+    friends_array.compact
+  end
+
+  def pending_friends
+    friendships.map{|friendship| friendship.friend if !friendship.confirmed}.compact
+  end
+
+  def friend_requests
+    friends.map{|friendship| friendship.user if !friendship.confirmed}.compact
+  end
+
+  def confirm_friend(user)
+    friendship = friends.find{|friendship| friendship.user == user}
+    friendship.confirmed = true
+    friendship.save
+  end
+
+  def friend?(user)
+    friends_list.include?(user)
+  end
 end
