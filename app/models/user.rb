7efc7c6c1b502f224f8f27176648615cc29bd175
jose-Abel <joseabel.ramirezfrontany@gmail.com>
2020-12-08 12:00:11 -0400

    Fix Rubocop errors of trailing whitespace line

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,12 +11,12 @@ class User < ApplicationRecord
   has_many :friendships
   has_many :inverted_friendships, class_name: 'Friendship', foreign_key: :friend_id
 
-  has_many :pending_friendships, -> { where confirmed: false }, class_name: "Friendship", foreign_key: "user_id" 
-  
+  has_many :pending_friendships, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'user_id'
+
   has_many :pending_friends, through: :pending_friendships, source: :friend
 
-  has_many :friendships_requests, -> { where confirmed: false }, class_name: "Friendship", foreign_key: "friend_id" 
-  
+  has_many :friendships_requests, -> { where confirmed: false }, class_name: 'Friendship', foreign_key: 'friend_id'
+
   has_many :friend_requests, through: :friendships_requests, source: :user
 
   def friends
